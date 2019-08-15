78ae29f043b03b38e7a486afa971bde7e3e5dafa
jpquiroga <jpquiroga@gmail.com>
2019-08-15 20:56:25 +0200

    First setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lemmatization-lists",
+    version="0.1",
+    packages=find_packages(),
+
+    # metadata to display on PyPI
+    author="jpquiroga",
+    author_email="jpquiroga@gmail.com",
+
+)
