75d0434f4d2c037a7bfe562e78e8ce873ed5ad0d
jesusdelpeso <df382ef8615a9b3fdd2d1343267c3f14edce82d8>
2019-08-29 10:56:56 +0200

    Added POS based lemmatizer for Spanish

--- src/lemmatization_lists/lemmatizers.py
+++ src/lemmatization_lists/lemmatizers.py
@@ -1,5 +1,18 @@
 from typing import Dict, List, Text
 from pkg_resources import resource_stream
+import unidecode
+
+
+def _normalize_word(word: Text) -> Text:
+    """
+    Normalize a wor:
+    - to lower case
+    - remove accents
+    :param word:
+    :return: The normalized word
+    """
+    return unidecode.unidecode(word.lower())
+
 
 class UnsupportedLanguageException(Exception):
     """
@@ -48,7 +61,7 @@ class Lemmatizer(object):
         self.lang = lang
         pass
 
-    def get_lemma(self, word: Text) -> List[Text]:
+    def get_lemma(self, word: Text, *args) -> List[Text]:
         """
         Get the lemma corresponding to a given word
         :param word:
@@ -66,6 +79,7 @@ class DictionaryLemmatizer(Lemmatizer):
     def __init__(self, lang: Text):
         super().__init__(lang)
         self.lemma_dict = self._build_dictionary(lang)
+        self.lemma_dict_norm = self._build_dictionary_norm(self.lemma_dict)
 
 #    def _get_lemmatization_list_file(self, lang: Text) -> Text:
 #        if lang not in self.SUPPORTED_LANGUAGES:
@@ -90,6 +104,54 @@ class DictionaryLemmatizer(Lemmatizer):
                 self._add_lemmatization_entry(lemma_dict, l[0], l[0])
         return lemma_dict
 
+    def _build_dictionary_norm(self, lemma_dict:Dict[Text, List[Text]]):
+        res = {}
+        for word, lemmas in lemma_dict.items():
+            _normalized_lemmas =[_normalize_word(w) for w in lemmas]
+            res[word] = _normalized_lemmas
+            res[_normalize_word(word)] = _normalized_lemmas
+        return res
+
     def get_lemma(self, word) -> List[Text]:
+        """
+        Get the lemmas corresponding to a word.
+        :param word:
+        :return: List of possible lemmas.
+        """
         return self.lemma_dict.get(word.lower(), [word.lower()])
 
+    def get_lema_norm(self, word) -> List[Text]:
+        """
+        Get the normalized lemmas corresponding to a word.
+        :param word:
+        :return: List of possible lemmas.
+        """
+        return self.lemma_dict_norm.get(word.lower(), [word.lower()])
+
+
+class SpanishPosLemmatizer(Lemmatizer):
+    """
+
+    """
+
+    def __init__(self):
+        self.dict_lemmatizer = DictionaryLemmatizer("es")
+        self.infinitives = self._read_verbs()
+
+    def get_lemma(self, word: Text, is_verb: bool) -> List[Text]:
+        lemmas = self.dict_lemmatizer.get_lemma(word)
+        if is_verb:
+            return [l for l in lemmas if l in self.infinitives]
+        else:
+            return [l for l in lemmas if l not in self.infinitives]
+
+    def _read_verbs(self):
+        res = set()
+        with resource_stream(__name__, "language/lemma/es/verb_data/verbs_list") as f:
+#        with open("src/lemmatization_lists/language/lemma/es/verb_data/verbs_list") as f:
+            for l in f.readlines():
+                _l = l.decode("utf8").strip()
+                if len(_l) > 0:
+                    res.add(_l)
+                    res.add(_normalize_word(_l))
+        return res
